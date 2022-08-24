64b5073ca944a3b054403c958bb96eaa8e5edc41
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-25 01:20:29 +0600

    вывод 2 элемента из трехзначного числа

---
+++ hw010/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int a = int.Parse(Console.ReadLine());
+//int N = a / 100;
+
+while (a < 100 || a >= 1000)
+{
+    Console.WriteLine("Введите трехзначное число ");
+    a = int.Parse(Console.ReadLine());
+}
+
+int num1 = a / 10;
+int num2 = num1 % 10;
+
+Console.WriteLine("Второй символ числа " + num2);
