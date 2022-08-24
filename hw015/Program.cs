1fdf565eaecba32db5070cf62f4e8a8ff3bf101f
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-25 01:27:05 +0600

    день недели выходной/рабочий

---
+++ hw015/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число от 1 до 7 ");
+int a = int.Parse(Console.ReadLine());
+
+while ( a <= 0 || a >= 8)
+    {
+    Console.WriteLine("Введите число от 1 до 7 ");
+    a = int.Parse(Console.ReadLine());
+    }
+
+if (a > 0 && a <= 5)
+{
+Console.WriteLine(a + " это рабочий день");
+}
+else 
+{
+Console.WriteLine(a + " это выходной день");
+}  
+    
